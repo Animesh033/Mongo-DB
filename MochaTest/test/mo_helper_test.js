557546869275e6b5e953ab0cab2f356d1a7c1ac1
Animesh <animesh.ultimate@gmail.com>
2021-01-27 22:45:17 +0530

    MongoDB Basics and code testing with Mocha Test Framework

---
+++ MochaTest/test/mo_helper_test.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+mongoose.Promise = global.Promise; //ES6 - promise
+
+before(done => {
+  mongoose.connect("mongodb://localhost/mongomocha", { useNewUrlParser: true, useUnifiedTopology: true });
+  mongoose.connection
+    .once("open", () => {
+      console.log("Connected");
+      done();
+    })
+    .on("error", error => {
+      console.log("Your Error", error);
+    });
+});
+
+beforeEach(done => {
+  mongoose.connection.collections.students.drop(() => {
+    console.log("Students drop was success!");
+    done();
+  });
+});
